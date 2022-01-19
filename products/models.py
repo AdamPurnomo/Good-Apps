39d11b2dec266cc6fda50f9d491b5f51687c62c3
Adam <adamsyammaszaki@gmail.com>
2022-01-19 08:12:13 +0700

    creating product on progress

--- products/models.py
+++ products/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    url = models.TextField()
+    votes_total = models.IntegerField(default=1)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        sumlist = self.body.split()[:50]
+        summ = ' '.join(sumlist) + '.....'
+        return summ
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
